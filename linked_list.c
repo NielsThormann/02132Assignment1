69ab944429e3fd616b411e17c4a2562747673712
Triids <mikkel.allermand@icloud.com>
2022-09-21 12:44:42 +0200

    Changed linked list to include x and y

--- linked_list.c
+++ linked_list.c
@@ -4,34 +4,27 @@
 // Stolen from https://www.learn-c.org/en/Linked_lists
 
 typedef struct node {
-    int val;
+    int x;
+    int y;
     struct node * next;
 } node_t;
-
-void construct_list() {
-    node_t * head = NULL;
-    head = (node_t *) malloc(sizeof(node_t));
-    if (head == NULL) {
-        return 1;
-    }
-    head->val = 1;
-    head->next = NULL;
-}
 
 void print_list(node_t * head) {
     node_t * current = head;
 
     while (current != NULL) {
-        printf("%d\n", current->val);
+        printf("%d\n", current->x, "%d\n", current->y);
         current = current->next;
     }
 }
 
-void push(node_t ** head, int val) {
+void push(node_t ** head, int x, int y) {
     node_t * new_node;
     new_node = (node_t *) malloc(sizeof(node_t));
 
-    new_node->val = val;
+    new_node->x = x;
+    new_node->y = y;
+
     new_node->next = *head;
     *head = new_node;
 }
